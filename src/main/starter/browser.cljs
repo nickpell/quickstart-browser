c7591cf948242cd04a296eeb6f2f8437e575b5c1
Nick Pellegrino <nickpell@users.noreply.github.com>
2022-05-10 16:58:29 -0400

    demo: getAuth is undefined

--- src/main/starter/browser.cljs
+++ src/main/starter/browser.cljs
@@ -1,4 +1,7 @@
-(ns starter.browser)
+(ns starter.browser
+  (:require
+    ["firebase/app" :refer (getAuth)]))
+
 
 ;; start is called by init and after code reloading finishes
 (defn ^:dev/after-load start []
@@ -9,7 +12,9 @@
   ;; this is called in the index.html and must be exported
   ;; so it is available even in :advanced release builds
   (js/console.log "init")
-  (start))
+  (start)
+  (js/console.log "getAuth:" getAuth))
+
 
 ;; this is called before any code is reloaded
 (defn ^:dev/before-load stop []
